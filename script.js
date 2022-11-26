aaf828d226f8f4e5cee1d5e9dcbb3322f848b3e7
Andrea <a.moscara9@gmail.com>
2022-11-26 16:47:38 +0100

    added eventlistener for when the mouse leaves a square the square turns yellow

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ for (i=0;i<256;i++) {
 let div = document.createElement('div');
 div.classList.add('square');
 document.querySelector('main').appendChild(div);
+div.addEventListener('mouseleave', () => {
+    div.classList.add('full');
+});
 };
+
