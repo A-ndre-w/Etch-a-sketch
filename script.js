53c81d99f8408b37f2fb693285dcae723dfdf07c
Andrea <a.moscara9@gmail.com>
2022-11-26 17:06:30 +0100

    added button with event listener; when clicked it resets the current grid and asks the user how many squares per row they want in the new grid

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-for (i=0;i<256;i++) {
-let div = document.createElement('div');
-div.classList.add('square');
-document.querySelector('main').appendChild(div);
-div.addEventListener('mouseleave', () => {
-    div.classList.add('full');
-});
+for (i=0;i<(256);i++) {
+    let div = document.createElement('div');
+    div.classList.add('square');
+    document.querySelector('main').appendChild(div);
+    div.addEventListener('mouseleave', () => {
+        div.classList.add('full');
+    });
 };
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const elements = document.getElementsByClassName('square');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    };
+    let squarenum = prompt('how many squares per row?');
+    for (i=0;i<(squarenum*squarenum);i++) {
+        let div = document.createElement('div');
+        div.classList.add('square');
+        document.querySelector('main').appendChild(div);
+        div.addEventListener('mouseleave', () => {
+            div.classList.add('full');
+        });
+    };
+})
+
+
+
 
